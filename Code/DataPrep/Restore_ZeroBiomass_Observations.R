f521a1475c254296e53a2cf7ee4c08e1a489f1c7
jee4my <jeremyc.jennings@gmail.com>
2020-11-08 15:00:06 -0800

    Annotated the code to describe what it's doing, added argument to replace NA's with zeroes

--- Code/DataPrep/Restore_ZeroBiomass_Observations.R
+++ Code/DataPrep/Restore_ZeroBiomass_Observations.R
@@ -1,12 +1,17 @@
 Raw_Data_2020_winter<- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataRaw/February 2020 SF McKenzie River.csv")
 
+##############
+## Subsetting the data from the Disturbed sample site 
 
 Raw_Biomass_winter_2020<- Raw_Data_2020_winter[,-c(1,10,13,14)]
 Disturb_BenthInsect_winter_2020<- subset(Raw_Biomass_winter_2020, Treatment=="Disturbed" & Substrate=="Benthic" & Insect=="insect" & Stage=="L")
 
+#########
+## Pivots by the Replicate column, fills in the biomass values, replaces NA's
+## with zeroes
 
 library(tidyr)
 Dist_Benth_Winter_2020_Wider<- Disturb_BenthInsect_winter_2020 %>%
-  pivot_wider(names_from = Replicate, values_from = Biomass)
+  pivot_wider(names_from = Replicate, values_from = Biomass, values_fill = 0)
 View(Disturb_BenthInsect_winter_2020)
 
