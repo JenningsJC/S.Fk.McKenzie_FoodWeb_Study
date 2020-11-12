f5eccd8ed6db817fb9c66ce0c51670d441e10a44
jee4my <jeremyc.jennings@gmail.com>
2020-11-11 19:12:33 -0800

    Added bootsrapping script called bootstrap_mean_annual_biomass.R and placed in Bootstrapping folder

---
+++ Code/Bootstrapping/bootstrap_mean_annual_biomass.R
@@ -0,0 +1,7 @@
+##################################################################################
+## bootstrap_mean_annual_biomass.R
+##
+## Script to generate bootstrap distributions of mean annual biomass and 95% CI's
+## from clean datasets.
+##
+###################################################################################
