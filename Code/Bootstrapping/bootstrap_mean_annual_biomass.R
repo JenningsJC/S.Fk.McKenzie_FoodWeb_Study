7e5790a794b3b1314a8d2fd231d6367e7340ffb1
jee4my <jeremyc.jennings@gmail.com>
2020-11-20 00:21:38 -0800

    started drafting bootstrap code, got nowhere, ready to hit myself in the head with a hammer

--- Code/Bootstrapping/bootstrap_mean_annual_biomass.R
+++ Code/Bootstrapping/bootstrap_mean_annual_biomass.R
@@ -1,9 +1,20 @@
-##################################################################################
+#####################################################################
 ## bootstrap_mean_annual_biomass.R
 ##
-## Script to generate bootstrap distributions of mean annual biomass and 95% CI's
-## from clean datasets.
+## Script to generate bootstrap distributions of mean annual biomass
 ##
-###################################################################################
+#####################################################################
+rm(list=ls())
 
-library(boot)
+dummy_benth_clean <-
+  read.csv(
+    "~/S.Fk.McKenzie_FoodWeb_Study/DataClean/dummy_benth_clean.csv"
+  )
+
+library(dplyr)
+library(tidyr)
+
+dummy_benth_clean_wider<- dummy_benth_clean %>%
+  pivot_wider(names_from = replicate, values_from = biomass)
+
+
