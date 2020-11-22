658b9bb7ca953d810fc3a6ed70b31b776c600412
jee4my <jeremyc.jennings@gmail.com>
2020-11-22 12:17:31 -0800

    clean up sections, add headings, add code to calc means from random sample, draft code for looping

--- Code/Bootstrapping/bootstrap_mean_annual_biomass.R
+++ Code/Bootstrapping/bootstrap_mean_annual_biomass.R
@@ -7,32 +7,55 @@
 #####################################################################
 rm(list=ls())
 
+install.packages("splitstackshape")
+library(splitstackshape)
+library(dplyr)
+library(tidyr)
+
+
 dummy_benth_clean <-
   read.csv(
     "~/S.Fk.McKenzie_FoodWeb_Study/DataClean/dummy_benth_clean.csv"
   )
 
-library(dplyr)
-library(tidyr)
+
 
 dummy_benth_clean_wider<- dummy_benth_clean %>%
   pivot_wider(names_from = season, values_from = biomass)
 
-
-# Calculate mean biomass, by season and taxon
-tapply(
-  dummy_benth_clean$biomass,
-  list(dummy_benth_clean$season, dummy_benth_clean$taxon),
-  mean
-)
-
-#take a stratified sample grouped by character vector of column names
+##################################################################
+## take a stratified random sample, grouped by character vector of 
+## specified columns
+##################################################################
 
 install.packages("splitstackshape")
 library(splitstackshape)
 
-stratified(
-  dummy_benth_clean,
-  c("taxon", "season", "biomass"), 1 ,
-  replace = TRUE
+boot_sample <- stratified(dummy_benth_clean,
+                          c("taxon", "season", "biomass"), 1 ,
+                          replace = TRUE)
+
+##################################################################
+## Calculate mean annual biomass for each taxon, from random samples of
+## seasonal replicates
+##################################################################
+
+tapply(
+  boot_sample$biomass,
+  list(boot_sample$taxon),
+  mean
 )
+
+##################################################################
+## apply stratified() in a loop
+##################################################################
+
+boot_sample <- data.frame()
+for(i in 1:4) {
+  Y<- stratified(
+    dummy_benth_clean,
+    c("taxon", "season", "biomass"), 1 ,
+    replace = TRUE
+  )
+
+}
