ca6d7781a5cf9cc5dd9bd887235e3e9de4d65d3a
jee4my <jeremyc.jennings@gmail.com>
2020-11-30 19:51:49 -0800

    clean up subheadings

--- Code/MakeBiomassGraphsTables/plot_means_CIs_of_bootstrap_annual_means.R
+++ Code/MakeBiomassGraphsTables/plot_means_CIs_of_bootstrap_annual_means.R
@@ -8,6 +8,7 @@ rm(list = ls())
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+
 boot_dist_annl_bioms_alpha <- read.csv(
   "~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/dummy_bootdistr_annual_means_alpha.csv"
 )
@@ -15,8 +16,11 @@ boot_dist_annl_bioms_alpha <- read.csv(
 boot_dist_annl_bioms_bravo <- read.csv(
   "~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/dummy_bootdistr_annual_means_bravo.csv"
 )
+########################################################
+## prep bootstrap distribution of means data for 
+## boxplotting by pivoting longer
+########################################################
 
-## prep means data for boxplotting by pivoting longer
 boot_dist_alpha_long <-
   pivot_longer(boot_dist_annl_bioms_alpha,
                1:3,
@@ -34,7 +38,10 @@ boot_dist_alpha_long$taxon <- as.factor(boot_dist_alpha_long$taxon)
 
 boot_dist_bravo_long$taxon <- as.factor(boot_dist_bravo_long$taxon)
 
-## create boxplots of boot distributions of annual means for each site, by taxon
+########################################################
+## create boxplots of boot distributions of annual means 
+## for each site, by taxon
+########################################################
 plot1 <-
   ggplot(boot_dist_alpha_long, aes(x = taxon, y = means, color = taxon)) + geom_boxplot()
 plot1 + stat_summary(
@@ -52,8 +59,10 @@ plot2 + stat_summary(
   shape = 1,
   size = 3
 )
-
-## creat boxplots of boot distributions of annual means by taxon, grouped by site
+########################################################
+## creat boxplots of boot distributions of annual means 
+## by taxon, grouped by site
+########################################################
 boot_dist_alpha_bravo <-
   rbind(boot_dist_alpha_long, boot_dist_bravo_long)
 
