35f2b995849787f40ea163debb57a919b20d86ab
jee4my <jeremyc.jennings@gmail.com>
2021-02-07 19:41:20 -0800

    created Figures folder, subset means for boxplotting in groups.

--- Code/MakeBiomassGraphsTables/plot_means_CIs_of_bootstrap_annual_means.R
+++ Code/MakeBiomassGraphsTables/plot_means_CIs_of_bootstrap_annual_means.R
@@ -18,16 +18,6 @@ disturb_benth_quantiles <-
   )
 
 
-########################################################
-## prep bootstrap distribution of means data for 
-## boxplotting by pivoting longer
-########################################################
-## 1:n below, n = number of taxa/columns in data frame
-bootdistro_disturbed_benth_long <-
-  pivot_longer(bootdistro_disturb_benth_annual_means,
-               1:229,
-               names_to = "Taxon",
-               values_to = "Means")
 
 
 #####################################################
@@ -46,15 +36,23 @@ disturb_benth_quantiles <- disturb_benth_quantiles %>%
 disturb_benth_quant_AllZero <- filter(disturb_benth_quantiles, mean == 0)
 disturb_benth_quant_NoZero <- filter(disturb_benth_quantiles, mean > 0)
 
-## Remove taxa with 95% CI that includes zero
+## Remove taxa with 95% CIs that include zero
 disturb_benth_quant_bigs<- filter(disturb_benth_quantiles, `2.5` > 0)
+
+## subset by range of mean
+upper <- subset(disturb_benth_quant_bigs, mean >= 10, select = c(taxon, mean, `97.5`, `2.5`))
+
+lower <- subset(disturb_benth_quant_bigs, mean <= 10 & mean >= 1, select = c(taxon, mean, `97.5`, `2.5`))
+
+lowest <- subset(disturb_benth_quant_bigs, mean <= 1, select = c(taxon, mean, `97.5`, `2.5`))
 
 ############################################################################
 ## Make bargraphs with error bars showing CI's
 ##
 ############################################################################
 
-plot1 <- ggplot(df3, disturb_benth_quant_bigs(x=taxon, y=mean)) + 
+
+ggplot(disturb_benth_quant_bigs, mapping = aes(x=taxon, y=mean)) + 
   geom_bar(stat="identity", position=position_dodge()) +
   geom_errorbar(aes(ymin= `2.5`, ymax= `97.5`), width=.2,
                 position=position_dodge(.9))
@@ -76,3 +74,34 @@ table1 <- table1 %>%
     subtitle = "By Taxon & Sample Site (mg/m^2)"
   )
 table1
+
+#######################################################
+## Read in csv of biitdistro of means
+######################################################
+bootdistro_disturb_benth_annual_means<-
+  read.csv(
+    "~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/bootdistr_annual_mean_disturbed_benth.csv"
+  )
+
+########################################################
+## prep bootstrap distribution of means data for 
+## boxplotting by pivoting longer
+########################################################
+## 1:n below, n = number of taxa/columns in data frame
+bootdistro_disturbed_benth_long <-
+  pivot_longer(bootdistro_disturb_benth_annual_means,
+               1:229,
+               names_to = "Taxon",
+               values_to = "Means")
+
+
+###########################################################
+## Make horizontal boxplots of distribution of annual means
+##
+##########################################################
+
+
+
+qplot(Taxon, Means, data = bootdistro_disturbed_benth_long, geom = "boxplot") +
+  coord_flip()
+temp1 <- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataRaw/raw_benth_wood_dat_allseasons_2019_2020.csv")
