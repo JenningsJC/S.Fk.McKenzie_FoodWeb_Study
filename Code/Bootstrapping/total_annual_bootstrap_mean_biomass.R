581041b251e0ed89e14f9f27eb7d4c258960e276
jee4my <jeremyc.jennings@gmail.com>
2021-02-10 21:57:52 -0800

    sum the total annual biomass for every bootstrap distribution, for every treatment/sample site

--- Code/Bootstrapping/total_annual_bootstrap_mean_biomass.R
+++ Code/Bootstrapping/total_annual_bootstrap_mean_biomass.R
@@ -59,20 +59,41 @@ bootdistr_annual_mean_relicchan_wood <-
 total_means_disturbed_benth <-
   bootdistr_annual_mean_disturbed_benth %>% mutate(sum = rowSums(.[1:229]))
 
+total_means_disturbed_wood <-
+  bootdistr_annual_mean_disturbed_wood %>% mutate(sum = rowSums(.[1:153]))
 
+total_means_floodforest_benth <-
+  bootdistr_annual_mean_floodforest_benth %>% mutate(sum = rowSums(.[1:229]))
+
+total_means_phase3_benth <-
+  bootdistr_annual_mean_phase3_benth %>% mutate(sum = rowSums(.[1:229]))
+
+total_means_phase4_benth <-
+  bootdistr_annual_mean_phase4_benth %>% mutate(sum = rowSums(.[1:229]))
+
+total_means_relic_chan_benth <-
+  bootdistr_annual_mean_relic_chane_benth %>% mutate(sum = rowSums(.[1:229]))
+
+total_means_relicchan_wood <-
+  bootdistr_annual_mean_relicchan_wood %>% mutate(sum = rowSums(.[1:153]))
 ####################################################################
 ## compute mean and 95% CI of bootstrap distribution of annual means
 ####################################################################
 
-tic()
-boot_means <- apply(annual_means, 2, mean)
-toc()
-means_of_bootdistro_of_means <- as.data.frame(boot_means)
+# calculate the mean of the distribution of total mean biomasses
+tot_disturb_benth_meanof_distro <-
+  mean(total_means_disturbed_benth[["sum"]])
 
-tic()
+
+
+
+
+
+# calculate the 95% CI using the percentile method
 quants <- c(0.975, 0.025)
-quants_of_bootdistro_of_means <-
-  apply(annual_means , 2 , quantile , probs = quants)
-toc()
+tot_disturb_benth_quantiles <-
+  apply(tot_disturb_benth_meanof_distro , 2 , quantile , probs = quants)
+
+
 quants_of_bootdistro_of_means <-
   as.data.frame(quants_of_bootdistro_of_means)
