9779ec24112edd5e5b84f4501313887a3d4b1706
jee4my <jeremyc.jennings@gmail.com>
2021-02-10 21:06:50 -0800

    sum across rows, and find the mean and 95% CI of the bootstrap distribution of total means (the 'sum' column for each treatment)

--- Code/Bootstrapping/total_annual_bootstrap_mean_biomass.R
+++ Code/Bootstrapping/total_annual_bootstrap_mean_biomass.R
@@ -18,7 +18,32 @@ library(forcats)
 ## Read-in bootdistro_means for each Treatment/sample site
 ##############################################################
 
-disturb_benth_quantiles <-
+bootdistr_annual_mean_disturbed_benth <-
   read.csv(
-    "~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/annual_meanquant_bootdistr_disturbed_benth.csv"
+    "~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/bootdistr_annual_mean_disturbed_benth.csv"
   )
+
+##############################################################
+## Sum across columns, row by row
+##############################################################
+
+total_means_disturbed_benth <-
+  bootdistr_annual_mean_disturbed_benth %>% mutate(sum = rowSums(.[1:229]))
+
+
+####################################################################
+## compute mean and 95% CI of bootstrap distribution of annual means
+####################################################################
+
+tic()
+boot_means <- apply(annual_means, 2, mean)
+toc()
+means_of_bootdistro_of_means <- as.data.frame(boot_means)
+
+tic()
+quants <- c(0.975, 0.025)
+quants_of_bootdistro_of_means <-
+  apply(annual_means , 2 , quantile , probs = quants)
+toc()
+quants_of_bootdistro_of_means <-
+  as.data.frame(quants_of_bootdistro_of_means)
