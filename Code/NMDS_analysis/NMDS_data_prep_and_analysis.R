60276737d3ba4e5970e66b658c330d89c6b17ba6
jee4my <jeremyc.jennings@gmail.com>
2021-05-12 00:34:41 -0700

    clean up and finish code through stressplot analysis

--- Code/NMDS_analysis/NMDS_data_prep_and_analysis.R
+++ Code/NMDS_analysis/NMDS_data_prep_and_analysis.R
@@ -27,43 +27,6 @@ pretreat_main_channel_biomass <- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataDer
 side_channel_biomass <- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/biomass_side_channel_2020.csv")
 wetted_forest_biomass <- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/biomass_wetted_forest_2020.csv")
 
-########################################
-# Square-root transform each reach
-########################################
-
-### Phase 3 ###
-# Add a column of square root transformed data
-phase3_biomass$sqrt.B <- sqrt(phase3_biomass$annual.B)
-
-# Remove untransformed column
-phase3_biomass_sqrt <- phase3_biomass[-2]
-
-### Phase 4 ###
-# Add a column of square root transformed data
-phase4_biomass$sqrt.B <- sqrt(phase4_biomass$annual.B)
-
-# Remove untransformed column
-phase4_biomass_sqrt <- phase4_biomass[-2]
-
-### Pre-treatment Main Channel ###
-# Add a column of square root transformed data
-pretreat_main_channel_biomass$sqrt.B <- sqrt(pretreat_main_channel_biomass$annual_B)
-
-# Remove untransformed column
-pretreat_main_channel_biomass_sqrt <- pretreat_main_channel_biomass[-2]
-
-### Side Channel ###
-# Add a column of square root transformed data
-side_channel_biomass$sqrt.B <- sqrt(side_channel_biomass$annual.B)
-
-# Remove untransformed column
-side_channel_biomass_sqrt <- side_channel_biomass[-2]
-
-### Wetted Forest ###
-# Add a column of square root transformed data 
-wetted_forest_biomass$sqrt.B <- sqrt(wetted_forest_biomass$annual.B)
-
-wetted_forest_biomass_sqrt <- wetted_forest_biomass[-2]
 
 #####################################
 ## Merge biomass data objects by taxa 
@@ -71,33 +34,33 @@ wetted_forest_biomass_sqrt <- wetted_forest_biomass[-2]
 ######################################
 
 # fulljoin phase 3 and 4 by taxon
-fulljoin_phase3_4 <- full_join(phase3_biomass_sqrt,phase4_biomass_sqrt, by = "taxon")
+fulljoin_phase3_4 <- full_join(phase3_biomass,phase4_biomass, by = "taxon")
 
 # rename biomass.x and biomass.y columns to 'phase3' & 'phase4'
 colnames(fulljoin_phase3_4)[2] = "Phase_3"
 colnames(fulljoin_phase3_4)[3] = "Phase_4"
 
 # join phase 3 & 4 with pretreat main channel biomass
-fulljoin_ph3_4_main_ch <- full_join(fulljoin_phase3_4, pretreat_main_channel_biomass_sqrt, by = "taxon" )
+fulljoin_ph3_4_main_ch <- full_join(fulljoin_phase3_4, pretreat_main_channel_biomass, by = "taxon" )
 
 #rename 'annual.B' column to 'pretreat main chan'
 colnames(fulljoin_ph3_4_main_ch)[4] = "Pretreat_main_ch"
 
 # join phase 3, 4, pretreat main channe,and side channel biomass
-ph3_4_main_ch_side_ch <- full_join(fulljoin_ph3_4_main_ch, side_channel_biomass_sqrt, by = "taxon" )
+ph3_4_main_ch_side_ch <- full_join(fulljoin_ph3_4_main_ch, side_channel_biomass, by = "taxon" )
 
 #rename 'annual.B' column to 'Side chan'
 colnames(ph3_4_main_ch_side_ch)[5] = "Side_chan"
 
 # join phase 3, 4, pretreat main chan side chan & wetted forest biomass
-ph3_4_main_ch_side_ch_forest <- full_join(ph3_4_main_ch_side_ch, wetted_forest_biomass_sqrt, by = "taxon" )
+ph3_4_main_ch_side_ch_forest <- full_join(ph3_4_main_ch_side_ch, wetted_forest_biomass, by = "taxon" )
 
 #rename 'annual.B' column to 'Side chan'
 colnames(ph3_4_main_ch_side_ch_forest)[6] = "Wetted_forest"
 
 
 # replace NA's with zeroes
-ph3_4_main_ch_side_ch_forest[is.na(ph3_4_main_ch_side_ch_forest)] = 0
+# ph3_4_main_ch_side_ch_forest[is.na(ph3_4_main_ch_side_ch_forest)] = 0
 
 # transpose to make taxa into columns and reaches into rows
 combo_biomass_transposed <- (t(ph3_4_main_ch_side_ch_forest[-1]))
@@ -120,13 +83,25 @@ write.csv(
   row.names = F
 )
 
-################################################
-## Convert square root transformed biomass 
-## matrix into Curtis-Bray distance matrix
-##
-################################################
+###############################################
+## Transform & Standardize biomass data
+##############################################
 
-biomass_dist_matrix <- vegdist(combo_biomass_transposed, method = "bray")
+##combo_biomass_norm <- decostand(combo_biomass_transposed, 'total', na.rm = T)
+
+# square root transform the data
+combo_biomass_sqrt<- sqrt(combo_biomass_transposed)
+
+# replace NA's with zeroes
+combo_biomass_sqrt[is.na(combo_biomass_sqrt)] = 0
+
+#Wisconsin double-standardization
+combo_biomass_wisc <- wisconsin(combo_biomass_sqrt)
+
+################################################
+## Convert into Curtis-Bray distance matrix
+################################################
+biomass_dist_matrix <- vegdist(combo_biomass_wisc, method = "bray")
 
 biomass_dist_matrix <- as.matrix(biomass_dist_matrix, labels = T)
 
@@ -136,16 +111,21 @@ biomass_dist_matrix <- as.matrix(biomass_dist_matrix, labels = T)
 ###
 ################################################
 
+  biomass_NMDS <-
+  metaMDS(
+    biomass_dist_matrix,
+    distance = "bray",
+    k = 3,
+    maxit = 999,
+    trymax = 500,
+    wascores = TRUE
+  )
 
-biomass_NMD <-
-  metaMDS(biomass_dist_matrix,
-          distance = "bray",
-          k = 2,
-          maxit = 999, 
-          trymax = 500,
-          wascores = TRUE)
+####
 
 
 
-stressplot(biomass_NMDS)
+bioNMS <- monoMDS(biomass_dist_matrix, k = 2, model = "global",  threshold = 0.8, maxit = 1000, weakties = TRUE, stress = 1, scaling = TRUE, pc = TRUE, smin = 1e-4, sfgrmin = 1e-7,  sratmax=0.99999) 
+goodness(bioNMS)
+stressplot(bioNMS)
 
