558aed4e029810781317940f59725a44f3f8e954
jee4my <jeremyc.jennings@gmail.com>
2021-05-22 22:51:20 -0700

    formatting

--- Code/NMDS_analysis/NMDS_data_prep_and_analysis.R
+++ Code/NMDS_analysis/NMDS_data_prep_and_analysis.R
@@ -36,40 +36,40 @@ wetted_forest_biomass <- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/bio
 ## and fill in the NA's with zeroes
 ######################################
 
-# fulljoin phase 3 and 4 by taxon
+## fulljoin phase 3 and 4 by taxon
 fulljoin_phase3_4 <- full_join(phase3_biomass,phase4_biomass, by = "taxon")
 
-# rename biomass.x and biomass.y columns to 'phase3' & 'phase4'
+## rename biomass.x and biomass.y columns to 'phase3' & 'phase4'
 colnames(fulljoin_phase3_4)[2] = "Phase_3"
 colnames(fulljoin_phase3_4)[3] = "Phase_4"
 
-# join phase 3 & 4 with pretreat main channel biomass
+## join phase 3 & 4 with pretreat main channel biomass
 fulljoin_ph3_4_main_ch <- full_join(fulljoin_phase3_4, pretreat_main_channel_biomass, by = "taxon" )
 
-#rename 'annual.B' column to 'pretreat main chan'
+## rename 'annual.B' column to 'pretreat main chan'
 colnames(fulljoin_ph3_4_main_ch)[4] = "Pretreat_main_ch"
 
-# join phase 3, 4, pretreat main channe,and side channel biomass
+## join phase 3, 4, pretreat main channe,and side channel biomass
 ph3_4_main_ch_side_ch <- full_join(fulljoin_ph3_4_main_ch, side_channel_biomass, by = "taxon" )
 
-#rename 'annual.B' column to 'Side chan'
+## rename 'annual.B' column to 'Side chan'
 colnames(ph3_4_main_ch_side_ch)[5] = "Side_chan"
 
-# join phase 3, 4, pretreat main chan side chan & wetted forest biomass
+## join phase 3, 4, pretreat main chan side chan & wetted forest biomass
 ph3_4_main_ch_side_ch_forest <- full_join(ph3_4_main_ch_side_ch, wetted_forest_biomass, by = "taxon" )
 
-#rename 'annual.B' column to 'Side chan'
+## rename 'annual.B' column to 'Side chan'
 colnames(ph3_4_main_ch_side_ch_forest)[6] = "Wetted_forest"
 
-# replace NA's with zeroes
+## replace NA's with zeroes
  ph3_4_main_ch_side_ch_forest[is.na(ph3_4_main_ch_side_ch_forest)] = 0
 
-# transpose to make taxa into columns and reaches into rows
+## transpose to make taxa into columns and reaches into rows
 combo_biomass_transposed <- (t(ph3_4_main_ch_side_ch_forest[-1]))
 
 colnames(combo_biomass_transposed) <- ph3_4_main_ch_side_ch_forest[, 1]
 
-########
+
 ########### prepping data for PcA
 taxa_list <- colnames(combo_biomass_transposed)
 colnames(combo_biomass_transposed) <- NULL
