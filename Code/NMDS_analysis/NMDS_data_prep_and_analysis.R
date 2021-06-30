fd53c81ad125b9ea2bde776477768146bfb8ecb6
jee4my <jeremyc.jennings@gmail.com>
2021-06-30 00:01:18 -0700

    code to make biplots

--- Code/NMDS_analysis/NMDS_data_prep_and_analysis.R
+++ Code/NMDS_analysis/NMDS_data_prep_and_analysis.R
@@ -29,49 +29,83 @@ seasonal_biomass <- read.csv("~/S.Fk.McKenzie_FoodWeb_Study/DataDerived/seasonal
 ## Transform & Standardize biomass data
 ##############################################
 
-seasonal_biomass2 <- column_to_rownames(seasonal_biomass, var = "X")
+seasonal_biomass <- column_to_rownames(seasonal_biomass, var = "X")
 
 # log transform using decostand
-seasonal_biomass_log<- decostand(seasonal_biomass[,-1], "log", na.rm = T)
+seasonal_biomass_log<- decostand(seasonal_biomass, "log", na.rm = T)
 
 # Standardize by proportion of row totals:
 # Divide each value in each row by the row total (total biomass)
 # so that each taxa biomass value becomes a proportion of total B
-seasonal_biomass_log_props <- decostand(seasonal_biomass_log, "total")
+seasonal_biomass_logged_transformed <- decostand(seasonal_biomass_log, "total")
 
 ################################################
-## Convert into Curtis-Bray distance matrix
-################################################
-biomass_dist_matrix <- vegdist(seasonal_biomass_log_props, method = "bray")
-
-biomass_dist_matrix <- as.matrix(biomass_dist_matrix, labels = T)
-
-
-################################################
-### NMDS with biomass matrix
+### NMDS analysis
 ###
 ################################################
 
   biomass_NMDS <-
   metaMDS(
-    biomass_dist_matrix,
+    seasonal_biomass_logged_transformed,
     distance = "bray",
     k = 2,
     trymax = 500,
-    wascores = TRUE,
+    wascores = T,
     
   )
 
-## Stressplot results to see how well represented the variables are by the
-## new, reduced variables
+## Create stressplot of results to see how well represented the original 
+## variables are by the new, reduced variables
 stressplot(biomass_NMDS)
 
+###################################
+## Graph biplot of reaches
+###################################
+
+## create a blank plot
+
+ordiplot(biomass_NMDS, type = "n", main = "Reaches")
+
+## add sites/reaches to the plot
+ordilabel(biomass_NMDS, display = "sites", font = 3, fill = "orange",
+          col = "black", scaling = scl)
+
+######################################
+## Graph biplot of intrinsic species
+## intrinsic = drives the pattern of
+## distibution of the reaches
+######################################
+
+## create a blank plot
+
+ordiplot(biomass_NMDS, type = "n", main = "Intrinsic Species")
+
+## find vectors of variables and p-values
+intrinsics <- envfit(biomass_NMDS, seasonal_biomass_logged_transformed, permutations = 999)
+
+## add species to the plot
+ordilabel(biomass_NMDS, display = "species", font = 2, priority = priSpp,
+          scaling = scl)
+
+## side-by-side biplots of sites and species separately 
+
+layout(matrix(1:2, ncol = 2))
+plot(biomass_NMDS, type = "n", scaling = scl)
+ordilabel(biomass_NMDS, display = "sites", font = 3, fill = "hotpink",
+          col = "blue", priority = priSite, scaling = scl)
+plot(biomass_NMDS, type = "n", scaling = scl)
+ordilabel(biomass_NMDS, display = "species", font = 2, priority = priSpp,
+          scaling = scl)
+layout(1)
+
+orditorp(biomass_NMDS,display="species",col="red",air=0.01)
+orditorp(biomass_NMDS,display="sites",cex=1.25,air=0.5)
+
+
+
 ## biplot results
-plot(biomass_NMDS)
+plot(biomass_NMDS, type = "t")
 
 
 
-bioNMS <- monoMDS(biomass_dist_matrix, k = 2, model = "global",  threshold = 0.7, maxit = 1000, weakties = TRUE, stress = 1, scaling = TRUE, pc = TRUE, smin = 1e-4, sfgrmin = 1e-7,  sratmax=0.99999) 
-goodness(bioNMS)
-stressplot(bioNMS)
-bioNMS
+
